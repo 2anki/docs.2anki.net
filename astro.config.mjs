b430c904c006d05b1e8b0acdee8391f69b294499
Alexander Alemayhu <alexander@alemayhu.com>
2023-11-05 08:01:01 +0100

    docs: add basic example

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,25 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: '2anki.net',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Getting started', link: '/guides/getting-started/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: '2anki.net',
+      social: {
+        github: 'https://github.com/withastro/starlight',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            { label: 'Introduction', link: '/guides/introduction' },
+            { label: 'Getting started', link: '/guides/getting-started/' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
 });
---
+++ src/content/docs/guides/introduction.md
@@ -0,0 +1,15 @@
+---
+title: What is 2anki.net?
+description: Introduction of 2anki.net
+---
+
+2anki.net is a free to use service and it is [open source project](https://github.com/2anki). 
+The hosted services allows you to create Anki flashcards fast.
+
+## Benefits of using 2anki.net
+
+We have support for all of the Anki note types. You can create basic cards, reversed cards, cloze deletions and input cards.
+
+Your media files like images and audio files will be embedded. We also have some minor support for using Math equations.
+
+The service is also mobile friendly and does not require you to install anything to run it.
